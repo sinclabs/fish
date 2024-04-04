f199be1e637142e5c7af7697a2aa8837c2995664
subbu.alagappan <subbu.alagappan@eqtpartners.com>
2024-04-04 09:05:36 +0200

    more configs and bun completions

--- config.fish
+++ config.fish
@@ -1,7 +1,3 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 set -x DENO_INSTALL /Users/subbu.alagappan/.deno
 set -x PATH $PATH "$HOME/.volta/bin"
 set -x PATH $PATH "/usr/local/bin"
@@ -10,8 +6,16 @@ set -x PATH $PATH "$HOME/.cargo/bin"
 set -x PATH $PATH "/opt/homebrew/opt/coreutils/libexec/gnubin"
 set -x PATH $PATH "$HOME/.local/bin"
 set -x PATH $PATH "$DENO_INSTALL/bin" 
+set -x PATH $PATH "$HOME/go/bin" 
+set -x BUN_INSTALL "$HOME/.bun"
+set -x PATH $BUN_INSTALL/bin $PATH
 
 set -x NPM_TOKEN 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
+
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+set -gx ANDROID_HOME $HOME/Library/Android/sdk
+set -x PATH $PATH:$ANDROID_HOME/emulator
+set -x PATH $PATH:$ANDROID_HOME/platform-tools
 
 set --global hydro_symbol_prompt 🔥
 set --global hydro_multiline true
@@ -21,12 +25,20 @@ set -x DOCKER_HOST unix:///$HOME/.docker/run/docker.sock
 
 set -x DBT_ENV_SF_ACCOUNT eqt.west-europe.azure
 
+set -gx EDITOR nvim
+
+# set --global ZELLIJ_AUTO_ATTACH true
+# set --global ZELLIJ_AUTO_EXIT true
+
 zoxide init fish | source
 alias c=clear
 alias cat=bat
 alias ll="exa -lh"
 alias grep=rg
 alias icat="kitty +kitten icat"
+alias vim="nvim"
+alias v="nvim"
+alias tmux="tmux -f \"$HOME/.config/tmux/tmux.conf\""
 alias cloud-proxy-staging="~/scripts/cloud-proxy-staging/cloud-proxy-staging.sh"
 alias cloud-proxy-prod="~/scripts/cloud-proxy-prod/cloud-proxy-prod.sh"
 alias cloud-cluster-test="~/scripts/cloud-cluster-test.sh"
@@ -38,12 +50,6 @@ source $HOME/.config/fish/docker-aliases.fish
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/subbu.alagappan/google-cloud-sdk/path.fish.inc' ]; 
-  . '/Users/subbu.alagappan/google-cloud-sdk/path.fish.inc';
-  source /Users/subbu.alagappan/google-cloud-sdk/path.fish.inc
-end
 
 # Binds option-up
 bind \e\[1\;5A history-token-search-backward
@@ -65,6 +71,11 @@ test -e ~/.profile && source ~/.profile
 test -e ~/.fish_abbrs.fish && source ~/.fish_abbrs.fish
 
 source ~/.config/fish/functions/post-exec-newline.fish
+
+# if status is-interactive
+#   # Commands to run in interactive sessions can go here
+#   eval (zellij setup --generate-auto-start fish | string collect)
+# end
 
 function postexec-source-profile --on-event fish_postexec
     set command_line (echo $argv | string collect | string trim)
@@ -107,4 +118,11 @@ function try-help-man --on-event fish_postexec
   end
 end
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/subbu.alagappan/google-cloud-sdk/path.fish.inc' ]; 
+  . '/Users/subbu.alagappan/google-cloud-sdk/path.fish.inc';
+  source /Users/subbu.alagappan/google-cloud-sdk/path.fish.inc
+end
+
 exit 0
+
